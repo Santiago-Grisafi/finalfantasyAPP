165b5f725194468a800f29f0b912d8490abff497
Santiago <sngrisa@gmail.com>
2023-06-06 18:25:27 -0300

    Fix alerts in app

--- src/app/components/home/results/results.component.ts
+++ src/app/components/home/results/results.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ResultsComponent {
 
-  @Input() results: any[] = [];
+  @Input() results: any[] = [{}];
   @Input() filter: string = "";
 
 }
--- src/app/components/home/search/search.component.ts
+++ src/app/components/home/search/search.component.ts
@@ -16,6 +16,8 @@ export class SearchComponent {
   data: any[] = [];
   selected: string = "";
   showAlert: boolean = false;
+  showErr: boolean = false;
+  term: string = "";
 
   @ViewChild('searchInput') searchInput!: ElementRef<any>;
   @Output() setData: EventEmitter<any[]> = new EventEmitter<any[]>();
@@ -23,17 +25,51 @@ export class SearchComponent {
   @Output() setInput: EventEmitter<string> = new EventEmitter<string>();
 
   searchData = () => {
-    if (this.selected != "characters" && this.searchInput.nativeElement.value.trim().length >= 1) {
-      this.monstersService.getMonstersByName(this.searchInput.nativeElement.value).subscribe((monsters: Monster[]) => {
-        this.setData.emit(monsters);
-        this.setFilter.emit("monsters");
-        this.setInput.emit(this.searchInput.nativeElement.value);
-      }, (err) => {return this.showAlert=true});
-    } else if (this.searchInput.nativeElement.value.trim().length >= 1) {
-      this.charactersService.getCharactersByName(this.searchInput.nativeElement.value).subscribe((characters: Character[]) => {
-        this.setData.emit(characters);
-        this.setFilter.emit("characters");
-      }, (err) => {return this.showAlert=true})
+    if (this.selected.trim().length == 0) {
+      this.showAlert = true;
     }
+    if (this.selected == "monsters" && this.searchInput.nativeElement.value.trim().length >= 1) {
+      this.monstersService.getMonstersByName(this.searchInput.nativeElement.value).subscribe((monsters: Monster[]) => {
+        if (monsters.length >= 1) {
+          this.setData.emit(monsters);
+          this.setFilter.emit("monsters");
+          this.setInput.emit(this.searchInput.nativeElement.value);
+          this.reset();
+          this.term = this.searchInput.nativeElement.value;
+          this.showAlert = false;
+          this.showErr = false;
+        } else {
+          this.showErr = true;
+          this.setData.emit([]);
+          this.term = this.searchInput.nativeElement.value;
+          this.showAlert = false;
+        }
+      });
+    } else if (this.searchInput.nativeElement.value.trim().length >= 1 && this.selected == "characters") {
+      this.charactersService.getCharactersByName(this.searchInput.nativeElement.value).subscribe((characters: Character[]) => {
+        if (characters.length >= 1) {
+          this.setData.emit(characters);
+          this.setFilter.emit("characters");
+          this.setInput.emit(this.searchInput.nativeElement.value);
+          this.reset();
+          this.term = this.searchInput.nativeElement.value;
+          this.showAlert = false;
+          this.showErr = false;
+        } else {
+          this.showErr = true;
+          this.setData.emit([]);
+          this.term = this.searchInput.nativeElement.value;
+          this.showAlert = false;
+        }
+      });
+    }
+  }
+
+  reset = () => {
+    return this.searchInput.nativeElement.value = "";
+  }
+
+  showErrors = (value: boolean) => {
+    return this.showAlert = value;
   }
 }
